34477833e2eae05f51c4bb0046149b3ad73ba80e
Ristirianto Adi <ristiriantoadi@gmail.com>
2023-09-17 15:02:54 +0700

    change background image on sign in page to library pictures

--- src/layouts/authentication/sign-in/index.js
+++ src/layouts/authentication/sign-in/index.js
@@ -32,7 +32,7 @@ import MDTypography from "components/MDTypography";
 import BasicLayout from "layouts/authentication/components/BasicLayout";
 
 // Images
-import bgImage from "assets/images/bg-sign-in-basic.jpeg";
+import bgImage from "assets/images/bg-sign-in-library.jpg";
 
 import { useNavigate } from "react-router-dom";
 
