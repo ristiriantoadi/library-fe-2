6a788ee59d8873a47dee3a4e97ca1c42d9bca7c2
Ristirianto Adi <ristiriantoadi@gmail.com>
2023-09-27 21:57:40 +0700

    set scrollable and macHeight for member info modal

--- src/layouts/member/InfoModal/index.js
+++ src/layouts/member/InfoModal/index.js
@@ -22,78 +22,80 @@ function InfoModal(props) {
       <MDBox
         bgColor="white"
         style={{
-          overflow: "scroll",
+          // overflow: "scroll",
           position: "absolute",
           top: "50%",
           left: "50%",
           transform: "translate(-50%, -50%)",
           width: "70%",
-          maxHeight: "90vh",
+          // maxHeight: "90vh",
           p: 4,
         }}
-        borderRadius="lg"
+        // borderRadius="lg"
       >
         <MDBox
-          // mx={2}
+          mx={2}
           py={3}
           px={2}
-          // mt={3}
+          mt={-3}
           mb={3}
           variant="gradient"
           bgColor="info"
-          // borderRadius="lg"
+          borderRadius="lg"
           coloredShadow="info"
         >
           <MDTypography id="modal-modal-title" variant="h6" color="white">
             Info Anggota
           </MDTypography>
         </MDBox>
-        <Grid mx={2} container spacing={2}>
-          <Grid item xs={12} md={4}>
-            <MDBox mb={3}>
-              <MDTypography variant="body2" fontWeight="bold">
-                Nama
-              </MDTypography>
-              <MDTypography variant="body2">{props.member.name}</MDTypography>
-            </MDBox>
-            <MDBox mb={3}>
-              <MDTypography variant="body2" fontWeight="bold">
-                Tanggal Lahir
-              </MDTypography>
-              <MDTypography variant="body2">{props.member.date}</MDTypography>
-            </MDBox>
-            <MDBox>
-              <MDTypography variant="body2" fontWeight="bold">
-                Email
-              </MDTypography>
-              <MDTypography variant="body2">{props.member.email}</MDTypography>
-            </MDBox>
+        <MDBox style={{ overflow: "scroll", maxHeight: "80vh" }}>
+          <Grid mx={2} container spacing={2}>
+            <Grid item xs={12} md={4}>
+              <MDBox mb={3}>
+                <MDTypography variant="body2" fontWeight="bold">
+                  Nama
+                </MDTypography>
+                <MDTypography variant="body2">{props.member.name}</MDTypography>
+              </MDBox>
+              <MDBox mb={3}>
+                <MDTypography variant="body2" fontWeight="bold">
+                  Tanggal Lahir
+                </MDTypography>
+                <MDTypography variant="body2">{props.member.date}</MDTypography>
+              </MDBox>
+              <MDBox>
+                <MDTypography variant="body2" fontWeight="bold">
+                  Email
+                </MDTypography>
+                <MDTypography variant="body2">{props.member.email}</MDTypography>
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <MDBox mb={3}>
+                <MDTypography variant="body2" fontWeight="bold">
+                  ID Member
+                </MDTypography>
+                <MDTypography variant="body2">{props.member.noId}</MDTypography>
+              </MDBox>
+              <MDBox mb={3}>
+                <MDTypography variant="body2" fontWeight="bold">
+                  Jenis Kelamin
+                </MDTypography>
+                <MDTypography variant="body2">{props.member.gender}</MDTypography>
+              </MDBox>
+              <MDBox>
+                <MDTypography variant="body2" fontWeight="bold">
+                  Nomor Telepon
+                </MDTypography>
+                <MDTypography variant="body2">{props.member.phoneNumber}</MDTypography>
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={4} pb={2}>
+              <img src={props.member.profilePicture} style={{ maxWidth: "300px" }}></img>
+            </Grid>
           </Grid>
-          <Grid item xs={12} md={3}>
-            <MDBox mb={3}>
-              <MDTypography variant="body2" fontWeight="bold">
-                ID Member
-              </MDTypography>
-              <MDTypography variant="body2">{props.member.noId}</MDTypography>
-            </MDBox>
-            <MDBox mb={3}>
-              <MDTypography variant="body2" fontWeight="bold">
-                Jenis Kelamin
-              </MDTypography>
-              <MDTypography variant="body2">{props.member.gender}</MDTypography>
-            </MDBox>
-            <MDBox>
-              <MDTypography variant="body2" fontWeight="bold">
-                Nomor Telepon
-              </MDTypography>
-              <MDTypography variant="body2">{props.member.phoneNumber}</MDTypography>
-            </MDBox>
-          </Grid>
-          <Grid item xs={12} sm={12} md={12} lg={12} xl={4} pb={2}>
-            <img src={props.member.profilePicture} style={{ maxWidth: "300px" }}></img>
-          </Grid>
-        </Grid>
-        <BasicTabs member={props.member}></BasicTabs>
+          <BasicTabs member={props.member}></BasicTabs>
+        </MDBox>
       </MDBox>
     </Modal>
   );
