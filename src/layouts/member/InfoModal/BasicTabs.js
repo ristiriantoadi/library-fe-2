6cd3f68515e3cf3e0216b529c71e45a1ca643588
Ristirianto Adi <ristiriantoadi@gmail.com>
2023-10-15 04:30:53 +0700

    get fee

--- src/layouts/member/InfoModal/BasicTabs.js
+++ src/layouts/member/InfoModal/BasicTabs.js
@@ -64,7 +64,7 @@ export default function BasicTabs(props) {
         <BorrowingHistoryPanel member={props.member} />
       </CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
-        <FineHistoryPanel />
+        <FineHistoryPanel member={props.member} />
       </CustomTabPanel>
     </Box>
   );
--- src/layouts/member/InfoModal/FIneHistoryPanel.js
+++ src/layouts/member/InfoModal/FIneHistoryPanel.js
@@ -1,13 +1,42 @@
 import MDBox from "components/MDBox";
 import DataTable from "examples/Tables/DataTable";
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import { privateAxios } from "UtilRequests/util-axios";
 
-function FineHistoryPanel() {
+function FineHistoryPanel(props) {
+  FineHistoryPanel.propTypes = {
+    member: PropTypes.object,
+  };
   const columns = [
-    { Header: "Jenis Denda", accessor: "name", align: "left" },
-    { Header: "Tanggal Denda", accessor: "memberId", align: "left" },
-    { Header: "Nominal", accessor: "email", align: "left" },
+    { Header: "Jenis Denda", accessor: "feeType", align: "left" },
+    { Header: "Tanggal Denda", accessor: "createTime", align: "left" },
+    { Header: "Nominal", accessor: "amount", align: "left" },
   ];
-  const rows = [];
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    console.log("member id", props.member["_id"]);
+    privateAxios
+      .get("/admin/fee", { params: { userId: props.member["_id"] } })
+      .then((response) => {
+        console.log(response);
+        let rows = response.data.map((fee) => {
+          return {
+            feeType: fee["feeType"],
+            createTime: new Date(fee["createTime"]).toDateString(),
+            amount: fee["amount"].toLocaleString("id-ID", {
+              style: "currency",
+              currency: "IDR",
+              minimumFractionDigits: 0, // Remove decimal places for whole numbers
+            }),
+          };
+        });
+        setRows(rows);
+      })
+      .catch((error) => {});
+  }, []);
+
   return (
     <MDBox>
       <DataTable
